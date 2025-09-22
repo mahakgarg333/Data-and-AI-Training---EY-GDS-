660c77084e21533e3b1dff1b498bb334f4d23828
Mahak Garg <97606529+mahakgarg333@users.noreply.github.com>
2025-09-22 14:04:18 +0530

    Create set.py

---
+++ Sep 22 /Daily Activities/set.py
@@ -0,0 +1,19 @@
+
+#Set
+students = {"Rahul","Priya","Arjun", "Rahul"}
+print(students)
+
+
+fruits ={"apple", "mango", "banana"}
+print("orange" in fruits)
+print ("apple" in fruits)
+
+set_a = {"Rahul", "Priya","Amit"}
+set_b = {"Amit", "Sneha", "Priya"}
+print(set_a & set_b)
+print(set_a - set_b)
+print(set_a | set_b)
+
+names = ["Rahul", "Priya", "Arjun", "Rahul"]
+unique_names = set(names)
+print(unique_names)
