6d20d214ba3d6dbdd22a187e0488b5e46029bf39
Mahak Garg <97606529+mahakgarg333@users.noreply.github.com>
2025-10-10 15:03:54 +0530

    Create producer.py

---
+++ Oct 10/producer.py
@@ -0,0 +1,20 @@
+import pika
+import json
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+
+channel.queue_declare(queue= "student_tasks")
+
+task = {
+    "student_id" : 101,
+    "action": "generate_certificate",
+    "email": "rahul@example.com"
+}
+
+channel.basic_publish(exchange='',
+                     routing_key='student_tasks',
+                     body=json.dumps(task)
+                     )
+print(" [x] Sent %r" % task)
+connection.close()
